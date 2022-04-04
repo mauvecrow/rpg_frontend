6b6b1aa96a2d83016ddbaf2062bc49e7b2ef21ac
Mauvecrow <64569782+mauvecrow@users.noreply.github.com>
2022-04-04 17:15:10 -0600

    setup basic routing. soley used for router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { CharacterSelectionComponent } from './character-selection/character-selection.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', component: WelcomeComponent},
+  {path: '', redirectTo: '/welcome', pathMatch: 'full'},
+  {path: 'character-selection', component: CharacterSelectionComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), HttpClientModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
